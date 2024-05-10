6624fb65db41ade1edfffb17b6f498b6a3463364
aryan <aryan.jand@gmail.com>
2024-05-10 06:48:00 -0700

    New changes

--- download.py
+++ download.py
@@ -1,7 +1,11 @@
 from pytube import Playlist, YouTube
+from pytube.exceptions import VideoUnavailable
+import time
 
 # Get the playlist URL from the environment variable
-playlist_url = "https://www.youtube.com/watch?v=2EYHZ2zUqTg&list=PLgtFJ5i1fDDcpItxvY_UQdvC3bEiNCbSQ"
+playlist_url = (
+    "https://www.youtube.com/playlist?list=PLHxIBAskzRO6X_ClqPjV2mMqTGPQ0OigP"
+)
 
 if not playlist_url:
     print("Error: Playlist URL not found in environment variables.")
@@ -11,16 +15,37 @@ if not playlist_url:
 playlist = Playlist(playlist_url)
 
 # Directory where you want to save the videos
-save_path = "/"
+save_path = "/data/JT-Videos"
 
-# Iterate over all videos in the playlist
-for video in playlist.video_urls:
-    # Create a YouTube object for the video
-    yt = YouTube(video)
 
-    # Get the highest resolution stream
-    stream = yt.streams.get_highest_resolution()
+downloaded_count = 0
 
-    print("Downloading:", yt.title)
-    # Download the video to the specified directory
-    stream.download(output_path=save_path)
+for index, video_url in enumerate(playlist.video_urls):
+    if index < 76:
+        continue  # Skip the first 76 videos
+    try:
+        # Create a YouTube object for the video
+        yt = YouTube(video_url)
+
+        # Get the highest resolution stream
+        stream = yt.streams.get_highest_resolution()
+
+        print("Downloading:", yt.title)
+        # Download the video to the specified directory
+        stream.download(output_path=save_path)
+
+        # Increment downloaded count
+        downloaded_count += 1
+
+        # Check if downloaded count is a multiple of 10
+        if downloaded_count % 10 == 0:
+            print("Sleeping for 15 minutes...")
+            time.sleep(600)  # Sleep for 10 minutes
+
+    except VideoUnavailable:
+        print("Video is unavailable. Skipping...")
+
+    except Exception as e:
+        print("An error occurred:", e)
+
+print("Download completed.")
